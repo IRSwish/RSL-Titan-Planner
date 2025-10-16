c71016d157d45d6025d6187d0f5b7c5f998c79b1
IRSwish <48881467+IRSwish@users.noreply.github.com>
2025-10-16 12:41:32 +0200

    Update fusions-config.js

--- fusions/fusions-config.js
+++ fusions/fusions-config.js
@@ -1,11 +1,47 @@
 // fusions-config.js
 window.fusions = {
+  Maria: {
+    name: "Sanguine Maria Fusion",
+    json: "Sanguine-Maria.json"
+  },
   Granyt: {
     name: "Granyt Doorkeep Fusion",
     json: "Granyt-Doorkeep.json"
   },
-  Maria: {
-    name: "Sanguine Maria Fusion",
-    json: "Sanguine-Maria.json"
+  KroKhad: {
+    name: "Donatello Fusion",
+    json: "Donatello.json"
+  },
+  KroKhad: {
+    name: "Kro'Khad The Throatripper Fusion",
+    json: "KroKhad-The-Throatripper.json"
+  },
+  Titus: {
+    name: "Titus Blackplume Fusion",
+    json: "Titus-Blackplume.json"
+  },
+  Tholin: {
+    name: "Tholin Foulbeard Fusion",
+    json: "Tholin-Foulbeard.json"
+  },
+  Ishiyama: {
+    name: "Ishiyama The Immovable Fusion",
+    json: "Ishiyama-The-Immovable.json"
+  },
+  Lysanthir: {
+    name: "Lysanthir Beastbane Fusion",
+    json: "Lysanthir-Beastbane.json"
+  },
+  Fabian: {
+    name: "Lord Entertainer Fabian Fusion",
+    json: "Lord-Entertainer-Fabian.json"
+  },
+  Esme: {
+    name: "Esme The Dancer Fusion",
+    json: "Esme-The-Dancer.json"
+  },
+  MadHatter: {
+    name: "Mad Hatter Fusion",
+    json: "Mad-Hatter.json"
   }
 };
