9c56401d863f8c34236806954147dc2d90bc6b2f
IRSwish <48881467+IRSwish@users.noreply.github.com>
2025-10-28 13:07:38 +0100

    - banner

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function loadAdsterraBanner() {
   invokeScript.src = '//www.highperformanceformat.com/598199983e93ba13e5325d01ffc8a9cc/invoke.js';
   bannerContainer.appendChild(invokeScript);
 }
-document.addEventListener('DOMContentLoaded', loadAdsterraBanner);
+// document.addEventListener('DOMContentLoaded', loadAdsterraBanner);
 
 // === Protection anti-redirections publicitaires ===
 (function() {
