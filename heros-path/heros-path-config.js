2ba40670ac80cba3888b7d788a0e92220626d3aa
IRSwish <48881467+IRSwish@users.noreply.github.com>
2025-11-06 00:11:31 +0100

    Old paths

--- heros-path/heros-path-config.js
+++ heros-path/heros-path-config.js
@@ -1,15 +1,43 @@
 // heros-path-config.js
 window.fusions = {
+  "swordmasters-path": {
+    name: "Swordmaster's Path",
+    json: "2025/swordmasters-path.json"
+  },
   "halloween-path-2025": {
     name: "Halloween Path",
-    json: "halloween-path-2025.json"
+    json: "2025/halloween-path.json"
   },
   "doorkeeps-path": {
     name: "Doorkeep's Path",
-    json: "doorkeeps-path.json"
+    json: "2025/doorkeeps-path.json"
   },
   "orcs-path": {
     name: "Orc's Path",
-    json: "orcs-path.json"
+    json: "2025/orcs-path.json"
+  },
+  "lost-path": {
+    name: "Lost Path",
+    json: "2025/lost-path.json"
+  },
+  "spring-path-2025": {
+    name: "Spring Path",
+    json: "2025/spring-path.json"
+  },
+  "afterparty-path": {
+    name: "Afterparty Path",
+    json: "2025/afterparty-path.json"
+  },
+  "alice-path": {
+    name: "Alice Path",
+    json: "2025/alice-path.json"
+  },
+  "dancers-path": {
+    name: "Dancer's Path",
+    json: "2025/dancers-path.json"
+  },
+  "winters-path-2025": {
+    name: "Winter's Path",
+    json: "2025/winters-path.json"
   }
 };
