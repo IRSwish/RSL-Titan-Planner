f20d1609a555d9b5e8cce726a1c12718a8dfb9e4
IRSwish <48881467+IRSwish@users.noreply.github.com>
2025-11-14 00:32:20 +0100

    add 2 titans

--- titan/titans-config.js
+++ titan/titans-config.js
@@ -3,5 +3,17 @@ window.fusions = {
   "Ace-Reporter": {
     name: "Ace Reporter Titan Event",
     json: "Ace-Reporter.json"
+  },
+  "Smart-Fella": {
+    name: "Smart Fella Titan Event",
+    json: "Smart-Fella.json"
+  },
+  "Demonslayer": {
+    name: "Demonslayer Titan Event",
+    json: "Demonslayer.json"
+  },
+  "Community-Weeks": {
+    name: "Community Weeks Titan Event",
+    json: "Community-Weeks.json"
   }
 };
