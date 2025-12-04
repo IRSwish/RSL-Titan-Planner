3c239584010594196605907a549c6aac80f04bb6
IRSwish <48881467+IRSwish@users.noreply.github.com>
2025-12-04 13:24:58 +0100

    path
    path

--- heros-path/heros-path-config.js
+++ heros-path/heros-path-config.js
@@ -2,42 +2,42 @@
 window.fusions = {
   "swordmasters-path": {
     name: "Swordmaster's Path",
-    json: "2025/swordmasters-path.json"
+    json: "/heros-path/2025/swordmasters-path.json"
   },
   "halloween-path-2025": {
     name: "Halloween Path",
-    json: "2025/halloween-path.json"
+    json: "/heros-path/2025/halloween-path.json"
   },
   "doorkeeps-path": {
     name: "Doorkeep's Path",
-    json: "2025/doorkeeps-path.json"
+    json: "/heros-path/2025/doorkeeps-path.json"
   },
   "orcs-path": {
     name: "Orc's Path",
-    json: "2025/orcs-path.json"
+    json: "/heros-path/2025/orcs-path.json"
   },
   "lost-path": {
     name: "Lost Path",
-    json: "2025/lost-path.json"
+    json: "/heros-path/2025/lost-path.json"
   },
   "spring-path-2025": {
     name: "Spring Path",
-    json: "2025/spring-path.json"
+    json: "/heros-path/2025/spring-path.json"
   },
   "afterparty-path": {
     name: "Afterparty Path",
-    json: "2025/afterparty-path.json"
+    json: "/heros-path/2025/afterparty-path.json"
   },
   "alice-path": {
     name: "Alice Path",
-    json: "2025/alice-path.json"
+    json: "/heros-path/2025/alice-path.json"
   },
   "dancers-path": {
     name: "Dancer's Path",
-    json: "2025/dancers-path.json"
+    json: "/heros-path/2025/dancers-path.json"
   },
   "winters-path-2025": {
     name: "Winter's Path",
-    json: "2025/winters-path.json"
+    json: "/heros-path/2025/winters-path.json"
   }
 };
